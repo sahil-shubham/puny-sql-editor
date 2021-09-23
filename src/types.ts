39ac280d3c4e0437d720ba3d216935f9fc9d75c3
sahil-shubham <sahil.shubham2000@gmail.com>
2021-09-23 21:55:18 +0530

    feat: added centralized types; added product types

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  categoryID: number;
+  discontinued: string;
+  productID: number;
+  productName: string;
+  quantityPerUnit: string;
+  reorderLevel: number;
+  supplierID: number;
+  unitPrice: number;
+  unitsInStock: number;
+  unitsOnOrder: string;
+};
