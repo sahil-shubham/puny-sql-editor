454c995e1f61d12a1f0f98a00b41f356ad6923ab
sahil-shubham <sahil.shubham2000@gmail.com>
2021-09-23 13:15:17 +0530

    feat:  setup react router, kept App.tsx for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
+import MainArea from "./views/MainArea";
+
+//----------Styled Components------------
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+`;
+
+//======================================
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <AppContainer>
+        <Switch>
+          <Route exact path="/" component={MainArea} />
+        </Switch>
+      </AppContainer>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/views/MainArea.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainArea() {
+  return <h1>Hello, World!</h1>;
+}
+
+export default MainArea;
