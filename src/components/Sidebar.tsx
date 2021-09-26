ca6384c277e4467ea1a913684ce80bf5fdf73610
sahil-shubham <sahil.shubham2000@gmail.com>
2021-09-26 18:09:01 +0530

    style: sidebar basic styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import * as Icon from "react-feather";
+import { Database, Home, Settings, Terminal } from "react-feather";
 import styled from "styled-components";
 
 //----------------Styled Components---------------
@@ -7,6 +7,34 @@ import styled from "styled-components";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+
+  max-height: 95vh;
+  position: sticky;
+  top: 1rem;
+
+  margin: 1rem;
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+
+  margin: 1rem 0;
+`;
+
+const Icon = styled.svg`
+  margin: 0 0.5rem 0 0;
 `;
 
 //================================================
@@ -14,8 +42,21 @@ const Container = styled.div`
 function Sidebar() {
   return (
     <Container>
-      <Icon.Home />
-      <Icon.Database />
+      <List>
+        <ListItem>
+          <Icon as={Home} /> Home
+        </ListItem>
+
+        <ListItem>
+          <Icon as={Database} /> Tables
+        </ListItem>
+
+        <ListItem>
+          <Icon as={Terminal} /> SQL
+        </ListItem>
+      </List>
+
+      <Icon as={Settings} />
     </Container>
   );
 }
