ab1fb4da0d60fadeb60986e462ad4c4894ea3978
sahil-shubham <sahil.shubham2000@gmail.com>
2021-09-29 19:07:04 +0530

    style: added global typography related styled components

---
+++ src/shared/Typography.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: 1rem 0;
+  text-transform: capitalize;
+`;
+
+const Heading = styled.h2`
+  margin: 1rem 0;
+  text-transform: capitalize;
+`;
+
+export { Title, Heading };
